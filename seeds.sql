ac8627fe7e7b179da0d60801d6b6e990d1447a72
Jackson Kent <117012325+jacko2611@users.noreply.github.com>
2023-02-16 22:47:01 +1100

    Update seeds.sql

--- seeds.sql
+++ seeds.sql
@@ -1,33 +1,31 @@
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Rose", "Green", 01, 1),
-       ("Esther", "Kent", 02, 1),
-       ("Jane", "Doe", 03, 3),
-       ("John", "Doe", 04, 6),
-       ("John", "Smith", 05, 4),
-       ("Jane", "Smith", 06, 2),
-       ("Jaye", "Green", 07, 1),
-       ("Mathew", "Nichols", 07, 1);
-       
-INSERT INTO department (name)
-VALUES ("Sales"),
-       ("Marketing"),
-       ("Finance"),
-       ("Human Resources"),
-       ("Legal"),
-       ("Engineering"),
-       ("Quality Assurance"),
-       ("Customer Service");
+USE employees_db;
 
-INSERT INTO role (title, salary, department_name)
-VALUES ("Sales Lead", 100000, "Sales"),
-       ("Salesperson", 80000, "Sales"),
-       ("Lead Engineer", 120000, "Engineering"),
-       ("Software Engineer", 100000, "Engineering"),
-       ("Account Manager", 80000, "Sales"),
-       ("Accountant", 65000, "Finance"),
-       ("Legal Team Lead", 80000, "Legal"),
-       ("Lawyer", 60000, "Legal");
-
-SELECT salary, title, department_name, first_name, last_name
-FROM role
-INNER JOIN employee ON role.id = employee.role_id
+INSERT INTO department
+    (name)
+VALUES
+    ('Sales'),
+    ('Engineering'),
+    ('Finance'),
+    ('Legal');
+INSERT INTO role
+    (title, salary, department_id)
+VALUES
+    ('Sales Lead', 100000, 1),
+    ('Salesperson', 80000, 1),
+    ('Lead Engineer', 150000, 2),
+    ('Software Engineer', 120000, 2),
+    ('Account Manager', 160000, 3),
+    ('Accountant', 125000, 3),
+    ('Legal Team Lead', 250000, 4),
+    ('Lawyer', 190000, 4);
+INSERT INTO employee
+    (first_name, last_name, role_id, manager_id)
+VALUES
+    ('John', 'Doe', 1, NULL),
+    ('Mike', 'Chan', 2, 1),
+    ('Ashley', 'Rodriguez', 3, NULL),
+    ('Kevin', 'Tupik', 4, 3),
+    ('Kunal', 'Singh', 5, NULL),
+    ('Malia', 'Brown', 6, 5),
+    ('Sarah', 'Lourd', 7, NULL),
+    ('Tom', 'Allen', 8, 7);
